c7a9b20c3fc7ced04c1f2da40ff06219598ca7a4
Hannah Danziger <hdanziger8@gmail.com>
2017-11-03 15:25:26 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -6,4 +6,12 @@ class Show < ActiveRecord::Base
   def self.most_popular_show
     Show.where(self.highest_rating).take
   end
+
+  def self.lowest_rating
+    Show.minimum(:rating)
+  end
+
+  def self.least_popular_show
+    Show.where(self.lowest_rating).take
+  end
 end
